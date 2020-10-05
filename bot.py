5856270bf7cb6d7b1723f59c7cacde5784119738
Kantemir <kan.shandir@gmail.com>
2020-10-05 22:50:18 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+import telebot
+import pyowm
+from pyowm.exceptions.api_response_error import NotFoundError
+
+bot = telebot.TeleBot('1255681490:AAFaTCHYuZOQA2teXDHiN7JVVNHjtsuxr4g')  # апи телеграмбота
+owm = pyowm.OWM('6b89460e9aa402f788f427ec417cb2a8', language="RU")
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Укажите название города")
+
+
+@bot.message_handler(content_types=['text'])
+def handle_message(message):
+    try:
+        observation = owm.weather_at_place(message.text)
+    except NotFoundError:
+        bot.send_message(message.chat.id, "Такой город не найден")
+    else:
+        w = observation.get_weather()
+        temp = w.get_temperature('celsius')['temp']
+        ans = ('В городе ' + message.text + ' сейчас ' + w.get_detailed_status()) + '\n'
+        ans += ('Температура воздуха около: ' + str(temp) + '°') + '\n\n'
+
+        if temp > 20:
+            ans = ans + 'Тепло, можно в футболке'
+        elif temp < 20:
+            ans = ans + 'Достаточно прохладно, оденьтесь потеплее'
+        elif temp < 10:
+            ans = ans + 'Очень холодно, наденьте куртку'
+
+        bot.send_message(message.chat.id, ans)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
